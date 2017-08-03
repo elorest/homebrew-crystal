f989f52d64e1634201a242a7cb8eff810cc7bf7d
Isaac Sloan <isaac@isaacsloan.com>
2017-08-02 21:43:28 -0600

    updated formula.

--- crelease.rb
+++ crelease.rb
@@ -1,6 +1,6 @@
 class Crelease < Formula
   desc "CRelease for managing versioning and tagging."
-  version "0.1.2"
+  version "0.1.3"
   homepage "https://github.com/elorest/crelease"
   url "https://github.com/elorest/CRelease/archive/v#{version}.tar.gz"
   sha256 "8cc2cacd7d1c78386438037bcb5fa4542244a876269ec88deb5875c8de8c236e"
