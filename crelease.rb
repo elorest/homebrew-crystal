2bdb272f7c9221dd73055a06e7a5aa77f8696385
Isaac Sloan <isaac@isaacsloan.com>
2017-08-13 10:34:41 -0600

    new version of crelease and better docs

--- crelease.rb
+++ crelease.rb
@@ -1,13 +1,13 @@
 class Crelease < Formula
   desc "CRelease for managing versioning and tagging."
-  version "0.1.3"
+  version "0.2.0"
   homepage "https://github.com/elorest/crelease"
   url "https://github.com/elorest/CRelease/archive/v#{version}.tar.gz"
-  sha256 "87eecd3bfb1e6bf0441695fc87c0d5ae7e4de0560b48af7416fe89ab33b3c68d"
+  sha256 "bfd543033dcd1c2021a3fdb7a329b99f44237224cd48b6072a2672305c920f4e"
   depends_on "crystal-lang"
 
   def install
-    system "shards install && make"
+    system "shards install && make build"
     bin.install "bin/crelease"
   end
 end
