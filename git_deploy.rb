207cd3adbbdd91cb568837e7f785bcb6864c84af
Isaac Sloan <isaac@isaacsloan.com>
2017-11-28 11:43:19 -0700

    better sha

--- git_deploy.rb
+++ git_deploy.rb
@@ -3,7 +3,7 @@ class GitDeploy < Formula
   version "0.1.2"
   homepage "https://github.com/elorest/cry"
   url "https://github.com/elorest/git_deploy/archive/v#{version}.tar.gz"
-  sha256 "2eae8e5bebf318c84193a33f9d33a58f5bee11cddba3cb77a3f8d58100329c0e"
+  sha256 "6976b5aa082a2b630f03219953f53db5c0d347d387b7fdb330a0a29d24653ba4"
   depends_on "crystal-lang"
 
   def install
