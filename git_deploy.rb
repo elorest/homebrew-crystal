f3978cfee2c3fa36ef256122a4a2eba550eb89f8
Isaac Sloan <isaac@isaacsloan.com>
2017-11-28 11:40:24 -0700

    updated sha on git_deploy and version

--- git_deploy.rb
+++ git_deploy.rb
@@ -1,6 +1,6 @@
 class GitDeploy < Formula
   desc "Automated setup for Heroku like git deploy."
-  version "0.1.1"
+  version "0.1.2"
   homepage "https://github.com/elorest/cry"
   url "https://github.com/elorest/git_deploy/archive/v#{version}.tar.gz"
   sha256 "2eae8e5bebf318c84193a33f9d33a58f5bee11cddba3cb77a3f8d58100329c0e"
