7141b829d9c98f13b0b2d3fb5e460828dc033ef7
Isaac Sloan <isaac@isaacsloan.com>
2018-10-02 14:55:53 -0700

    upddated cry formula and rsa

--- cry.rb
+++ cry.rb
@@ -1,9 +1,9 @@
 class Cry < Formula
   desc "Pry like eval for crystal."
-  version "0.4.0"
+  version "0.5.0"
   homepage "https://github.com/elorest/cry"
   url "https://github.com/elorest/cry/archive/v#{version}.tar.gz"
-  sha256 "90879faaf5b7cf311a422ba1f5568ad0bd6ba1a019632bbbfe5f67e293e8a265"
+  sha256 "350aa269f96f7bbc4a2cdd9f2a8fef72047fef23d2b7ac0d82a2db8ba3788e0a"
   depends_on "crystal-lang"
 
   def install
